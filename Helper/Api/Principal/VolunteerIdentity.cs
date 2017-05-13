23b1fef9fbbe73343045163619e2663b4b2c634a
happinessandlove <1565803020@qq.com>
2017-05-13 21:13:33 +0800

    清理所有的文件

---
+++ Helper/Api/Principal/VolunteerIdentity.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+
+namespace System.Security.Principal
+{
+    public class VolunteerIdentity:IIdentity
+    {
+        public VolunteerIdentity(Volunteer volunteer)
+        {
+            this.VolunteerId = volunteer.Id;
+            this.Name = volunteer.MobileNumber;
+        }
+        public Guid VolunteerId
+        {
+            get;private set;
+        }
+        public string AuthenticationType
+        {
+            get { return "CustomAuthentication"; }
+        }
+        public string Name
+        {
+            get;
+        }
+
+        public bool IsAuthenticated
+        {
+            get;
+        }
+    }
+}
---
+++ Helper/Api/Principal/VolunteerPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace System.Security.Principal
+{
+    public class VolunteerPrincipal:IPrincipal
+    {
+        public VolunteerPrincipal(VolunteerIdentity identity)
+        {
+            this.Identity = identity;
+        }
+       
+        public VolunteerPrincipal(Volunteer volunteer)
+            :this(new VolunteerIdentity(volunteer))
+        {
+
+        }
+        public VolunteerIdentity Identity { get; }
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        IIdentity IPrincipal.Identity
+        {
+            get { return Identity; } 
+        }
+    }
+}
